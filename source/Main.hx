7d7ffa4b0d85fb4154fb5a63167c760ab17b3dc7
Vladimir Vlaicu <93855040+Vovo4k@users.noreply.github.com>
2023-06-01 16:16:48 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,7 +1,7 @@
 import flixel.FlxGame;
+import flixel.FlxG;
 import openfl.display.Sprite;
 import sys.io.File;
-import CharacterSelectionState;
 import MenuState;
 
 class Main extends Sprite {
@@ -11,12 +11,7 @@ class Main extends Sprite {
         var fileName: String = "assets/data/person/nick.json";
         var fileContent: String = File.getContent(fileName);
 
-        if (fileContent != null && fileContent != "") {
-            // Ник уже сохранен, переключаемся в меню
-            addChild(new FlxGame(0, 0, MenuState));
-        } else {
-            // Ник не сохранен, переключаемся в состояние ввода ника
-            addChild(new FlxGame(0, 0, CharacterSelectionState));
-        }
+        // Всегда переключаемся в меню
+        addChild(new FlxGame(0, 0, MenuState));
     }
 }
